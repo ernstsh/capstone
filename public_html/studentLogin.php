180ded601c10e8b5f92f05244a59168b85c5f823
jfranc34 <you@example.com>
2017-01-26 18:02:02 -0800

    student login dropdown list now have student names and camp names

--- public_html/studentLogin.php
+++ public_html/studentLogin.php
@@ -7,13 +7,61 @@ enrolled in that camp-->
 	</head>
 	<body>
 		<form action="" method="post">
-			<p>Select your camp</p>
-			<select name="camp">
-				<option selected="selected">Camp</option>
-			</select><br>
-			<p>Find your name</p>
+			<p>Select your name </p>
 			<select name="student">
-				<option selected="selected">Name</option>
+				<option selected="selected">--Select</option>
+                                <?php
+                                        //Connect to database 
+                                        $dbhost = 'oniddb.cws.oregonstate.edu';
+                                        $dbname = 'nichokyl-db';
+                                        $dbuser = 'nichokyl-db';
+                                        $dbpass = '1hvHqfNBEOL6iwL9';
+                                        $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+                                            or die("Error connecting to database server");
+
+                                        mysql_select_db($dbname, $mysql_handle)
+                                            or die("Error selecting database: $dbname");
+                                        //Get first name and last name from Responder table 
+                                        $query = "SELECT first_name, last_name FROM Responder";
+                                        $result = mysql_query($query);  
+                                        
+                                        //Fills dropdown list with the name and last name of each student 
+                                        while ($row = mysql_fetch_array($result)) 
+                                        {
+                                                echo "<option>" . $row{'first_name'} . " " .$row{'last_name'}. "</option>";
+                                        }
+                                        
+                                        //Close connection to database 
+                                        mysql_close($mysql_handle);
+                                ?>
+			</select><br>
+			<p>Find your camp</p>
+			<select name="camp">
+				<option selected="selected">--Select</option>
+                                  <?php
+                                        //Connect to database 
+                                        $dbhost = 'oniddb.cws.oregonstate.edu';
+                                        $dbname = 'nichokyl-db';
+                                        $dbuser = 'nichokyl-db';
+                                        $dbpass = '1hvHqfNBEOL6iwL9';
+                                        $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+                                            or die("Error connecting to database server");
+
+                                        mysql_select_db($dbname, $mysql_handle)
+                                            or die("Error selecting database: $dbname");
+                                        //Get first name and last name from Responder table 
+                                        $query = "SELECT title FROM Camp";
+                                        $result = mysql_query($query);  
+                                        
+                                        //Fills dropdown list with the name and last name of each student 
+                                        while ($row = mysql_fetch_array($result)) 
+                                        {
+                                                echo "<option>" . $row{'title'} . "</option>";
+                                        }
+                                        
+                                        //Close connection to database 
+                                        mysql_close($mysql_handle);
+                                ?>
 			</select><br>
 			<input type="submit" value="Submit">
 		</form>
