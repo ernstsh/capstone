088807887f5ed5424d49cda46d474d940a498cb2
jfranc34 <you@example.com>
2017-04-25 02:14:13 -0700

    Fixed drop down bug in edit/create a report page

--- public_html/SelectReportChoice.js
+++ public_html/SelectReportChoice.js
@@ -5,10 +5,7 @@ function GetReports(){
                 //Deletes the drop down options 
                 for(var x = 0; x < NumChildren-1; x++){
                         parentDropDown.removeChild(parentDropDown.lastChild); 
-                }
-        
-                //document.getElementById("dummy").innerHTML = "Getting Reports";
-                
+                }                
                 //For sending the javascript variable and for receiving the reports stored in the database 
                 var request= new XMLHttpRequest();
                 var choice = "Hello";
@@ -17,33 +14,14 @@ function GetReports(){
                 request.send(choice);
                 request.onreadystatechange=function(){
                         if(request.readyState == 4){
-                                if(request.status == 200){                                                                            
-                                        //alert(request.responseText);                                                                             
-                                        //document.getElementById("dummy3").innerHTML = request.responseText;
-                                        
-                                        //turns a string of JSON text into a Javascript object 
-                                        MultiArray = JSON.parse(request.responseText);
-                                        //document.getElementById("dummy3").innerHTML = MultiArray[10][2];
-                                        //document.getElementById("dummy3").innerHTML = MultiArray;
-
-                                        
-                                        //turns the string report JSON into JSON text 
-                                        var json = JSON.parse(MultiArray[1][2]);
-                                        //document.getElementById("dummy").innerHTML = json[0].label;
-                                        //document.getElementById("dummy2").innerHTML = MultiArray[0][1];
-                                        
-                                        //Gets the number of report rows 
-                                        var length = MultiArray.length;
-                                        //alert(length);
+                                if(request.status == 200){                                                                                                                 
+                                        var MultiArray = JSON.parse(request.responseText);                                      
                                         //Adds the names of the reports to the dropdown and the id set to the report_id
-                                        for(var x = 0; x < length; x++){
-                                                
+                                        for(var x = 0; x < MultiArray.length; x++){                                               
                                                 var optionReport = document.createElement("option");
                                                 optionReport.setAttribute("id", MultiArray[x][0]);
                                                 var textReport = document.createTextNode(MultiArray[x][1]);
-                                                //alert(MultiArray[x][1]);
                                                 optionReport.appendChild(textReport);
-                                                //parentDropDown = document.getElementById("selection");
                                                 parentDropDown.appendChild(optionReport);                                           
                                        }                                       
                                 }	
